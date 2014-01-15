1bb434dca1db07011e4ca1a87031d3eae85ae8a9
Ryan Lelek <ryanlelek@gmail.com>
2014-01-14 21:59:02 -0800

    Adding NPM install and uninstall scripts

---
+++ npm.modules.uninstall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#####################
+##### Uninstall #####
+#####################
+
+npm uninstall -g bower;
+npm uninstall -g coffee-script;
+npm uninstall -g express;
+npm uninstall -g forever;
+npm uninstall -g generator-angular;
+npm uninstall -g generator-karma;
+npm uninstall -g grunt-cli;
+npm uninstall -g hexo;
+npm uninstall -g jshint;
+npm uninstall -g localtunnel;
+npm uninstall -g looseleaf;
+npm uninstall -g mocha;
+npm uninstall -g pm2;
+npm uninstall -g recess;
+npm uninstall -g recursive-readdir;
+npm uninstall -g sails;
+npm uninstall -g should;
+npm uninstall -g supervisor;
+npm uninstall -g tutor;
+npm uninstall -g twilio;
+npm uninstall -g yo;
+npm uninstall -g zombie;
