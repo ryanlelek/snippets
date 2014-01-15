0868167a218433cf4272103877ee968d0e0b462e
Ryan Lelek <ryanlelek@gmail.com>
2014-01-14 22:03:34 -0800

    Removing version restrictions when installing NPM modules.

--- npm.modules.install.sh
+++ npm.modules.install.sh
@@ -4,25 +4,25 @@
 ##### Install #####
 ###################
 
-npm install -g bower@1.2.8;
-npm install -g coffee-script@1.6.3;
-npm install -g express@3.4.8;
-npm install -g forever@0.10.11;
-npm install -g generator-angular@0.7.0;
-npm install -g generator-karma@0.6.0;
-npm install -g grunt-cli@0.1.11;
-npm install -g hexo@2.4.5;
-npm install -g jshint@2.4.1;
-npm install -g localtunnel@0.2.2;
-npm install -g looseleaf@0.4.4;
-npm install -g mocha@1.17.0;
-npm install -g pm2@0.7.0;
-npm install -g recess@1.1.9;
-npm install -g recursive-readdir@0.0.2;
-npm install -g sails@0.9.8;
-npm install -g should@2.1.1;
-npm install -g supervisor@0.5.6;
-npm install -g tutor@0.4.0;
-npm install -g twilio@1.5.0;
-npm install -g yo@1.1.2;
-npm install -g zombie@2.0.0-alpha24;
+npm install -g bower;				# 1.2.8
+npm install -g coffee-script;		# 1.6.3
+npm install -g express;				# 3.4.8
+npm install -g forever;				# 0.10.11
+npm install -g generator-angular;	# 0.7.0
+npm install -g generator-karma;		# 0.6.0
+npm install -g grunt-cli;			# 0.1.11
+npm install -g hexo;				# 2.4.5
+npm install -g jshint;				# 2.4.1
+npm install -g localtunnel;			# 0.2.2
+npm install -g looseleaf;			# 0.4.4
+npm install -g mocha;				# 1.17.0
+npm install -g pm2;					# 0.7.0
+npm install -g recess;				# 1.1.9
+npm install -g recursive-readdir;	# 0.0.2
+npm install -g sails;				# 0.9.8
+npm install -g should;				# 2.1.1
+npm install -g supervisor;			# 0.5.6
+npm install -g tutor;				# 0.4.0
+npm install -g twilio;				# 1.5.0
+npm install -g yo;					# 1.1.2
+npm install -g zombie;				# 2.0.0-alpha24
