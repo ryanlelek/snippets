996cf76880e8f31ca8ca07fbe0cc0f5034ecc60b
Ryan Lelek <ryanlelek@gmail.com>
2014-05-24 00:05:13 -0700

    Adding and Upgrading node modules

--- node/npm.modules.install.sh
+++ node/npm.modules.install.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
+# For Scripting
+# ├─┬ module@1.0.0
+# └── last@0.0.1
+
 ###################
 ##### Install #####
 ###################
 
-npm install -g bower;				# 1.2.8
-npm install -g coffee-script;		# 1.6.3
-npm install -g express;				# 3.4.8
-npm install -g forever;				# 0.10.11
-npm install -g generator-angular;	# 0.7.0
-npm install -g generator-karma;		# 0.6.0
-npm install -g grunt-cli;			# 0.1.11
-npm install -g gulp;
-npm install -g hexo;				# 2.4.5
-npm install -g jshint;				# 2.4.1
-npm install -g localtunnel;			# 0.2.2
-npm install -g looseleaf;			# 0.4.4
-npm install -g mocha;				# 1.17.0
-npm install -g pm2;					# 0.7.0
-npm install -g recess;				# 1.1.9
-npm install -g recursive-readdir;	# 0.0.2
-npm install -g sails;				# 0.9.8
-npm install -g should;				# 2.1.1
-npm install -g supervisor;			# 0.5.6
-npm install -g tutor;				# 0.4.0
-npm install -g twilio;				# 1.5.0
-npm install -g yo;					# 1.1.2
-npm install -g zombie;				# 2.0.0-alpha24
+npm install -g bower;             # 1.3.3
+npm install -g coffee-script;     # 1.7.1
+npm install -g express;           # 4.3.1
+npm install -g firebase-tools;    # 1.0.4
+npm install -g forever;           # 0.11.1
+npm install -g generator-angular; # 0.8.0
+npm install -g generator-karma;   # 0.7.0
+npm install -g grunt-cli;         # 0.1.13
+npm install -g gulp;              # 3.6.2
+npm install -g hexo;              # 2.6.0
+npm install -g jshint;            # 2.5.1
+npm install -g localtunnel;       # 1.3.0
+npm install -g looseleaf;         # 0.4.4
+npm install -g mocha;             # 1.19.0
+npm install -g modulus;           # 1.0.0
+npm install -g node-gyp;          # 0.13.1
+npm install -g node-inspector;    # 0.7.3
+npm install -g pm2;               # 0.8.12
+npm install -g recess;            # 1.1.9
+npm install -g recursive-readdir; # 1.1.1
+npm install -g reggie;            # 0.2.1
+npm install -g sails;             # 0.9.16
+npm install -g should;            # 3.3.2
+npm install -g strider;           # 1.4.5
+npm install -g supervisor;        # 0.6.0
+npm install -g tutor;             # 0.5.0
+npm install -g twilio;            # 1.6.0
+npm install -g yo;                # 1.1.2
+npm install -g zombie;            # 2.0.0-alpha31
