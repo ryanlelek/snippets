3620fa46f9b66f6da920d084b74647e6c9409636
Ryan Lelek <ryanlelek@gmail.com>
2014-05-24 01:23:31 -0700

    Adding lodash, underscore, and fsevents to global NPM modules

--- node/npm.modules.install.sh
+++ node/npm.modules.install.sh
@@ -16,6 +16,7 @@ npm install -g coffee-script;     # 1.7.1
 npm install -g express;           # 4.3.1
 npm install -g firebase-tools;    # 1.0.4
 npm install -g forever;           # 0.11.1
+npm install -g fsevents;          # 0.2.0
 npm install -g generator-angular; # 0.8.0
 npm install -g generator-karma;   # 0.7.0
 npm install -g grunt-cli;         # 0.1.13
@@ -23,6 +24,7 @@ npm install -g gulp;              # 3.6.2
 npm install -g hexo;              # 2.6.0
 npm install -g jshint;            # 2.5.1
 npm install -g localtunnel;       # 1.3.0
+npm install -g lodash;            # 2.4.1
 npm install -g looseleaf;         # 0.4.4
 npm install -g mocha;             # 1.19.0
 npm install -g modulus;           # 1.0.0
@@ -38,5 +40,6 @@ npm install -g strider;           # 1.4.5
 npm install -g supervisor;        # 0.6.0
 npm install -g tutor;             # 0.5.0
 npm install -g twilio;            # 1.6.0
+npm install -g underscore;        # 1.6.0
 npm install -g yo;                # 1.1.2
 npm install -g zombie;            # 2.0.0-alpha31
